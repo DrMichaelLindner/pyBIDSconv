2c284e898eea12891cf2898b64918b4ce44cad7a
DrMichaelLindner <mlindner.mail@gmx.de>
2018-10-01 09:46:36 +0100

    new example pyBIDSconv_default.py file

--- example_files/pyBIDSconv_defaults.py
+++ example_files/pyBIDSconv_defaults.py
@@ -1,3 +1,4 @@
 
-default_categorization_file = 'C:\pyBIDSconv\example_files\pyBVIDSconv_catagorisation_Prisma.txt'
-default_config_file = 'C:\\pyBIDSconv\example_files\pyBIDSconv_config.py'
+default_categorization_file = r'C:\pyBIDSconv\example_files\pyBVIDSconv_catagorisation_Prisma.txt'
+default_config_file = r'C:\pyBIDSconv\example_files\pyBIDSconv_config.py'
+default_BIDS_folder = r'C:\yourBIDSfolder'
